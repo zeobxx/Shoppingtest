ef5e49fce4860cc82c9bcf5533515ef270939bb7
Phornchanok <Phronchanok27@gmail.com>
2021-02-20 22:59:59 +0700

    test

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,25 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+      
+    <Cart />
+   
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Cart from './components/Cart'
 
 export default {
-  name: 'App',
+  name: 'app',
   components: {
-    HelloWorld
+    Cart
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+  font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
--- src/main.js
+++ src/main.js
@@ -1,6 +1,10 @@
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue/dist/bootstrap-vue.css'
 import Vue from 'vue'
 import App from './App.vue'
+import BootstrapVue from 'bootstrap-vue'
 
+Vue.use(BootstrapVue)
 Vue.config.productionTip = false
 
 new Vue({
